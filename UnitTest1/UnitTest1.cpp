83464a2b4ce6e1e8e44fb9748a702b53d60376dd
KrasuliakDmytro <krasuliak.dmytro.kn@dspu.edu.ua>
2025-11-23 17:18:45 +0200

    my first test

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../labs5_1/lab05_1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t = h(2, 3);
+			Assert::AreEqual(t, 5);
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			double t = h(2, 3);
+			double expected = (3 * 3 - sqrt(2 * 2 + 3 * 3)) / (1 + 2 * 2 * 3 * 3);
+
+			Assert::AreEqual(expected, t, 1e-6);
+		}
+	};
+}
